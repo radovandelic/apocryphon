608049711508142d3f35e818245703a62f398b0c
kayacahit <cahit.kaya@elium.academy>
2017-11-22 21:01:47 +0100

    added style to the frontpage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Level, Languages } from './containers';
 import { connect } from 'react-redux';
+
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render = () => {
     var { level, language } = this.props;     // redux
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Your current language: {language}</h1>
-          <div className="App-title">Your current level: {level}</div>
+      <div className="app">
+        <header className="header">
+        <div className="left">
+          <img className="logo" alt=" "></img>
+          <a href='/' className="name">apocryphon</a>
+        </div>
+        <a href='/' className="right">
+          Login
+        </a>
         </header>
-        <div className="App-intro">
-          <Languages />
-          <Level />
+
+        <div className="content">
+          <div className="left">
+            <button className="button word active">demure</button>
+            <button className="button word">deride</button>
+            <button className="button word">diligent</button>
+            <button className="button word">elated</button>
+            <button className="button word">eloquent</button>
+            <button className="button word">embezzle</button>
+            <button className="button word">erudite</button>
+            <button className="button word">erudite</button>
+            <button className="button word">feral</button>
+            <button className="button word">extol</button>
+          </div>
+          <div className="center">
+            <div className="title">Some title</div>
+            <h1 className="current-word">DEMURE</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2O4U1BhYSDu4aPavo_237sC0w0r2eDgVKHVjkDPFhjvnywFDn" className="current-image" alt=" "></img>
+            <div className="inputs">
+              <input id="current-word-guess"  className="input" placeholder="add the translated word"></input>
+              <button type="button" className="button">Need a hint?</button>
+              <button type="button" className="button">Submit</button>
+            </div>
+          </div>
+          <div className="right">
+            <Languages />
+            <Level />
+            right sidebar
+          </div>
         </div>
       </div>
     );
