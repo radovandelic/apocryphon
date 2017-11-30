d06f780bdd712eb6ba12cd5ef2cfcde229a8a6f3
ivoryblakk <george.ingram1@outlook.com>
2017-11-30 19:36:31 +0100

    sesssion login-out

--- server/api/users/routes.js
+++ server/api/users/routes.js
@@ -1,26 +1,8 @@
 var router = require('express').Router();
 var User = require('./model');
 var bcrypt = require('bcrypt');
-const passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-router.post('/create', (req, res) => {
-  var user = req.body;
-  console.log(req.body);
-  bcrypt.hash(user.password, 8, (err, hash) => {
-    if (err) {
-      res.status(500).json(err);
-    }
-    user.password = hash;
-    User.create(user)
-      .then(user => {
-        res.status(200).json(user);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });
-});
+var session = require('client-sessions');
+var passport = require('passport');
 
 router.post('/create', (req, res) => {
   var user = req.body;
@@ -51,7 +33,7 @@ router.get('/:id', (req, res) => {
 });
 
 // U //
-router.post('/:id/update', (req, res) => {
+router.post('/:id/update', requireLogin, (req, res) => {
   var user = req.body;
   if (user.password) {
     bcrypt.hash(user.password, 8, (err, hash) => {
@@ -80,7 +62,7 @@ router.post('/:id/update', (req, res) => {
 });
 
 // D //
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id/delete', requireLogin, (req, res) => {
   User.remove({ _id: req.params.id }, err => {
     if (err) res.status(500).json(err);
     else res.status(200).json({ status: 'success' });
@@ -88,10 +70,7 @@ router.delete('/:id/delete', (req, res) => {
 });
 
 // login //
-router.post('/login', passport.authenticate('login',{
-  
-}, (req, res) => {
-  console.log('passport is working', req.user);
+router.post('/login', (req, res) => {
   console.log('sessions:', req.session);
   var email = req.body.email;
   var password = req.body.password;
@@ -107,6 +86,7 @@ router.post('/login', passport.authenticate('login',{
             console.log(user.password);
             if (result) {
               req.session.user = user;
+
               res.status(200).json(user);
               console.log(req.session);
             } else
@@ -122,31 +102,28 @@ router.post('/login', passport.authenticate('login',{
   });
 });
 
-router.get('/user/loggedin', (req, res) => {
-  res.send(req.isAuthenticated() ? req.user : '0');
-});
-
 router.post('/logout', (req, res) => {
+  console.log('this is sesson', req.session.user);
+  console.log('this is req.user', req.user);
   req.session.reset();
-  req.logout();
 
   res.status(200).json('you have logged out');
 });
-//  router.post('/login', (req, res, next) => {
-// passport.authenticate('local', {
-//   successRedirect: res.status(200).json({ status: 'success' }),
-//   failureRedirect:res
-//   .status(404)
-//   .json({ status: 'email/password combo not found' }),
-//   failureFlash: true
-// })(req,res,next);
-// });
+
 function isLoggedIn(req, res, next) {
-      // if user is authenticated in the session, carry on 
-      if (req.isAuthenticated())
-          return next();
-      // if they aren't redirect them to the home page
-      res.status(404).json({ status: 'Your are not logged in.' });
+  // if user is authenticated in the session, carry on
+  if (req.session && req.session.user) return next();
+  // if they aren't redirect them to the home page
+  res.status(404).json({ status: 'Your are not logged in.' });
+}
+
+function requireLogin(req, res, next) {
+  if (!req.user) {
+    console.log(req.user);
+    res.status(404).json({ status: 'Your are not logged in.' });
+  } else {
+    next();
   }
+}
 
 module.exports = router;
