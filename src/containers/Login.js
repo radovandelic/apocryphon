2fad42a7cc70466cc1d721ac3a96b1abb6a4fbe2
kayacahit <cahit.kaya@elium.academy>
2017-12-03 20:35:52 +0100

    form validation tests

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,43 +3,105 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-export default class Login extends Component {
+import { connect } from 'react-redux';
+
+class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            validation: null
         }
     }
     findUser = () => {
+        var { isLoggedIn } = this.props;
         var user = {}
         user.email = this.state.email;
         user.password = this.state.password;
-        console.log(user);
         axios.post(`http://localhost:8080/user/login`, user, { withCredentials: true })
             .then(res => {
-                console.log(res)
-
+                console.log(res);
+                if (res.status === 200) {
+                    isLoggedIn(res.data);
+                    this.setState({ validation: 'valid' })
+                } else {
+                    this.setState({ validation: 'invalid' })
+                }
             })
             .catch(error => console.log(error.response))
     }
+    emailValidation = (val) => {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        var label = document.getElementById('label-email');
+        var input = document.getElementById('email');
+        if (re.test(val)) {
+            console.log("valid")
+            label.classList.add('valid');
+            input.classList.add('valid');
+
+            label.classList.remove('invalid');
+            input.classList.remove('invalid');
+        }
+        else {
+            console.log("INvalid")
+            label.classList.add('invalid');
+            input.classList.add('invalid');
+
+            label.classList.remove('valid');
+            input.classList.remove('valid');
+        }
+
+    }
+    passwordValidation = (val) => {
+        var label = document.getElementById('label-password');
+        var input = document.getElementById('password');
+        if (val.length > 7) {
+            console.log("valid")
+            label.classList.add('valid');
+            input.classList.add('valid');
+
+            label.classList.remove('invalid');
+            input.classList.remove('invalid');
+        }
+        else {
+            console.log("INvalid")
+            label.classList.add('invalid');
+            input.classList.add('invalid');
+
+            label.classList.remove('valid');
+            input.classList.remove('valid');
+        }
+    }
     render() {
-        //var { languages, match, words } = this.props;  
-        console.log(this.state.email)
-        console.log(this.state.password)
+        var { login } = this.props;
+        console.log(login)
         return (
             <div className='content login'>
                 <h1>Login</h1>
                 <form>
-                    <label htmlFor='email'>Email</label>
-                    <input id='email' type='text'
+                    <label id='label-email' className='label' htmlFor='email'>Email</label>
+                    <input id='email' className='input email' type='email'
                         placeholder='Enter your email'
-                        onChange={e => this.setState({ email: e.target.value })} />
+                        onChange={e => {
+                            var val = e.target.value;
+                            this.setState({ email: val });
+                            this.emailValidation(val);
+                        }
+                        }
+                    />
 
-                    <label htmlFor='password'>Password</label>
-                    <input id='password' type='password'
+                    <label id='label-password' className='label' htmlFor='password'>Password</label>
+                    <input id='password' className='input password' type='password'
                         placeholder='Enter your password'
-                        onChange={e => this.setState({ password: e.target.value })} />
+                        onChange={e => {
+                            var val = e.target.value;
+                            this.setState({ password: e.target.value });
+                            this.passwordValidation(val);
+                        }
+                        }
+                    />
 
                     <button onClick={this.findUser} type='button' className='button'>Login</button>
                 </form>
@@ -48,3 +110,33 @@ export default class Login extends Component {
         )
     }
 }
+
+const LoginFailed = () => {
+
+    return (
+        <h4>Email or Password wrong.</h4>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        login: state.login,
+        level: state.level
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    isLoggedIn(data) {
+        dispatch({
+            type: "IS_LOGGED_IN",
+            data
+        })
+    }
+})
+
+Login = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Login)
+
+export default Login;
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
             username: null,
-            email: null,
-            password: null,
-            successful: false
+            email: '',
+            password: '',
+            successful: false,
+            validEmail: false,
+            validPassword: false
         }
+    }
+    componentDidMount = () => {
+        document.getElementById('email').focus();
+        document.getElementById('password').focus();
+        this.enableSubmitButton();
+    }
+    componentDidUpdate = () => {
+        setTimeout(() => {
+            document.getElementById('email').focus();
+            this.enableSubmitButton();
+        }, 1500)
     }
     createUser = () => {
         var user = {}
@@ -30,17 +43,64 @@ export default class Register extends Component {
             })
             .catch(error => console.log(error.response))
     }
-    formValidationEmail = () => {
-        if (!this.state.email) {
-            return " is required";
+    emailValidation = (val) => {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        var label = document.getElementById('label-email');
+        var input = document.getElementById('email');
+        if (re.test(val)) {
+            this.setState({ validEmail: true });
+
+            label.classList.add('valid');
+            input.classList.add('valid');
+
+            label.classList.remove('invalid');
+            input.classList.remove('invalid');
         }
+        else {
+            this.setState({ validEmail: false });
+
+            label.classList.add('invalid');
+            input.classList.add('invalid');
+
+            label.classList.remove('valid');
+            input.classList.remove('valid');
+        }
+        this.enableSubmitButton();
     }
-    formValidationPassword = () => {
-        var password = this.state.password;
-        if (password) {
-            if (String(password).length < 8) {
-                return " (8 characters or more)";
-            }
+    passwordValidation = (val) => {
+        var label = document.getElementById('label-password');
+        var input = document.getElementById('password');
+        if (val.length > 7) {
+            this.setState({ validPassword: true });
+
+            label.classList.add('valid');
+            input.classList.add('valid');
+
+            label.classList.remove('invalid');
+            input.classList.remove('invalid');
+        }
+        else {
+            this.setState({ validPassword: false });
+
+            label.classList.add('invalid');
+            input.classList.add('invalid');
+
+            label.classList.remove('valid');
+            input.classList.remove('valid');
+        }
+        this.enableSubmitButton();
+    }
+
+    enableSubmitButton = () => {
+        var validEmail = this.state.validEmail;
+        var validPassword = this.state.validPassword;
+
+        var submitButton = document.getElementById('submit-button');
+
+        if (validEmail && validPassword) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
         }
     }
     render() {
@@ -60,26 +120,44 @@ export default class Register extends Component {
                 <h4>
                     Choose a Username, Email and a password to create a new account.
                 </h4>
-                <form>
+                <form ref='form'>
                     <label htmlFor='username'>Username</label>
-                    <input id='username' type='text'
+                    <input id='username' className='input' type='text'
                         placeholder='Choose a username'
                         onChange={e => this.setState({ username: e.target.value })}
                     />
 
-                    <label htmlFor='email'>Email {this.formValidationEmail()}</label>
-                    <input id='email' type='text'
+                    <label id='label-email' className='label' htmlFor='email'>Email</label>
+                    <input autocpmplete='off' id='email' className='input email' type='email'
+                        value={this.state.email}
                         placeholder='Enter your email'
-                        onChange={e => this.setState({ email: e.target.value })}
+                        onChange={e => {
+                            var val = e.target.value;
+                            this.setState({ email: val });
+                            this.emailValidation(val);
+                        }
+                        }
+                        onFocus={
+                            this.focussed
+                        }
                     />
 
-                    <label htmlFor='user'>Password {this.formValidationPassword()}</label>
-                    <input id='user' type='password'
+                    <label id='label-password' className='label' htmlFor='password'>Password</label>
+                    <input id='password' className='input password' type='password'
+                        value={this.state.password}
                         placeholder='Enter your password'
-                        onChange={e => this.setState({ password: e.target.value })}
+                        onChange={e => {
+                            var val = e.target.value;
+                            this.setState({ password: e.target.value });
+                            this.passwordValidation(val);
+                        }
+                        }
+                        onFocus={
+                            this.focussed
+                        }
                     />
 
-                    <button onClick={this.createUser} type='button' className='button'>Sign Up</button>
+                    <button id='submit-button' onClick={this.createUser} type='button' className='button disabled'>Sign Up</button>
                 </form>
                 Already have an account? <Link to='/login'>Login here.</Link>
             </div>
